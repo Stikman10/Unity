3c32a1f57fca9cedec588346cbc11683e149ccf1
Eduardo <90112959+Stikman10@users.noreply.github.com>
2021-09-30 22:25:15 -0600

    Add files via upload

---
+++ VariablesAndOperations.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Testing VariablesAndOperations//
+public class VariableAndOperations : MonoBehavious
+{
+    int myInt = 7;
+
+    void Start()
+    {
+        myInt = MultiplyByThree(myInt);
+        Debug.Log(myInt);
+    }
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = *2
+                return result;
+    } }
+
+//Testing VariableAndOperations 2 //
+public class VariableAndOperations : Monobehaviour
+{
+    int myInt = 3;
+    void start ()
+    {
+        myInt = MultiplyByFour(myInt);
+        Debug.Log(myInt)
+
+            int DivideByTwo(int number)
+        {
+            int result;
+            result = number * 4;
+            return result;
+        }
+    }
+}
