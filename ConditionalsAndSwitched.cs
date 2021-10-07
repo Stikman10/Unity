c7a6ddde878de2c3435e2b39cda48cb5bc19368a
Eduardo <90112959+Stikman10@users.noreply.github.com>
2021-10-06 19:18:17 -0600

    Add files via upload

---
+++ ConditionalsAndSwitched.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int intelligence = 5;
+
+    void Greet ()
+    {
+        switch (intellgience)
+        {
+            case 5:
+                print("Hello, do you know about the high elfs");
+                break;
+            case 4:
+                print ("Hello")
+                    break;
+            case 3:
+                print ("Not in the mood mate, piss off")
+                    break;
+            case 2:
+                print ("Get out of my sight before I crack open your skull")
+                    break;
+            case 1:
+                print ("stab")
+break;
+            default: }
+    
+
+     }
